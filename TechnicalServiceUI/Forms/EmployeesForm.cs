b7c0904e0a755cddf3812520e0e923e656c559ea
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-04-24 16:33:51 +0300

    ...

--- TechnicalServiceUI/Forms/EmployeesForm.cs
+++ TechnicalServiceUI/Forms/EmployeesForm.cs
@@ -1,4 +1,7 @@
-﻿using DevExpress.XtraEditors;
+﻿using Business.Abstract;
+using Business.DependencyResolvers.AutoFac;
+using DevExpress.XtraEditors;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,14 +16,42 @@ namespace TechnicalServiceUI.Forms
 {
     public partial class EmployeesForm : DevExpress.XtraEditors.XtraForm
     {
+        private readonly IEmployeeService _employeeService;
+        private readonly IDepartmentService _departmentService;
         public EmployeesForm()
         {
             InitializeComponent();
+            _employeeService = InstanceFactory.GetInstance<IEmployeeService>();
+            _departmentService = InstanceFactory.GetInstance<IDepartmentService>();
         }
 
         private void EmployeesForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnAddEmployee_Click(object sender, EventArgs e)
+        {
+            var result = _employeeService.Add(new Employee
+            {
+                FirstName = txtFirstName.Text,
+                LastName = txtLastName.Text,
+                Address = memoAddress.Text,
+                DateOfBirth = Convert.ToDateTime(deDateOfBirth.Text),
+                Email = txtEmailAddress.Text,
+                IdentityNumber = txtIdentityNumber.Text,
+                Phone = txtPhoneNumber.Text,
+                Picture = System.IO.File.ReadAllBytes(pictureEditEmployeePicture.GetLoadedImageLocation()),
+                DepartmentId = Convert.ToInt32(lueDepartmentName.EditValue.ToString()),
+
+            });
+        }
+
+        private void GetAllDepartments()
+        {
+            lueDepartmentName.Properties.DataSource = _departmentService.GetAll().Data;
+            lueDepartmentName.Properties.DisplayMember = "DepartmentName";
+            lueDepartmentName.Properties.ValueMember = "DepartmentId";
         }
     }
 }
