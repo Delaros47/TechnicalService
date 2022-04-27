0a0f1548605dce30881b3b98c5e7ee95212d4182
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-04-27 22:25:49 +0300

    ....

--- TechnicalServiceUI/Forms/MainForm.cs
+++ TechnicalServiceUI/Forms/MainForm.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using TechnicalServiceUI.Project_Reports;
+using TechnicalServiceUI;
 
 namespace TechnicalServiceUI.Forms
 {
--- TechnicalServiceUI/Program.cs
+++ TechnicalServiceUI/Program.cs
@@ -1,4 +1,5 @@
 using TechnicalServiceUI.Forms;
+using TechnicalServiceUI;
 
 namespace TechnicalServiceUI
 {
